8998d10da60e48c5a2cfae87a00564ed5bab1a8c
Javid KH. Ibrahimli <JavidKHi@CODE.LOCAL>
2019-09-22 19:47:17 +0400

    willnext

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+    items:1,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true
+  });
+  });
