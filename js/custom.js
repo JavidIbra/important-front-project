48225c9d444648762e14f9072beace3c6cf9fd51
Javid KH. Ibrahimli <JavidKHi@code.local>
2019-09-26 21:26:08 +0400

    willnext

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,40 @@
 $(document).ready(function(){
-  $('.owl-carousel').owlCarousel({
+  
+  $('#coruselHero').owlCarousel({
     items:1,
     loop:true,
     margin:10,
     autoplay:true,
+    nav: true,
+    dots: true,
     autoplayTimeout:3000,
-    autoplayHoverPause:true
+    autoplayHoverPause:true,
+    navText: ['<span class="icon-arrow_back">', '<span class="icon-arrow_forward">'],
+    smartSpeed: 1000
   });
+  $('#corusel1').owlCarousel({
+    items:3,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    nav: true,
+    dots: true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    navText: ['<span class="icon-arrow_back">', '<span class="icon-arrow_forward">'],
+    smartSpeed: 1000
   });
+
+
+  });
+
+  /*
+  items: 1,
+				loop: true,
+				margin: 0,
+				autoplay: true,
+				nav: true,
+				dots: true,
+				navText: ['<span class="icon-arrow_back">', '<span class="icon-arrow_forward">'],
+				smartSpeed: 1000
+  */
