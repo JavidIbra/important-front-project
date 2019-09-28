0ad8bcfe58267f5c6c725630a3a359a8fba3e13d
JavidIbra <53233368+JavidIbra@users.noreply.github.com>
2019-09-28 18:55:41 +0400

    40%
    Signed-off-by: JavidIbra <53233368+JavidIbra@users.noreply.github.com>

--- js/custom.js
+++ js/custom.js
@@ -24,6 +24,18 @@ $(document).ready(function(){
     navText: ['<span class="icon-arrow_back">', '<span class="icon-arrow_forward">'],
     smartSpeed: 1000
   });
+  $('#corusel2').owlCarousel({
+    items:3,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    nav: true,
+    dots: true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    navText: ['<span class="icon-arrow_back">', '<span class="icon-arrow_forward">'],
+    smartSpeed: 1000
+  });
 
 
 
