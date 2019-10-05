752bb7a0cddb0c54d9d1adf0259d03f87d2aebe2
JavidIbra <53233368+JavidIbra@users.noreply.github.com>
2019-10-05 13:14:47 +0400

    willbe
    Signed-off-by: JavidIbra <53233368+JavidIbra@users.noreply.github.com>

--- js/sticky.js
+++ js/sticky.js
@@ -1,7 +1,38 @@
+window.addEventListener('load', windowYuklendi);
+
+
+function windowYuklendi() {
+
+    window.addEventListener('scroll', function (melumat) {
+        var navTop = document.querySelector('nav.top-bar');
+
+        if (window.scrollY >= 110) {
+            navTop.classList.add('fixed');
+        }
+        else{
+            navTop.classList.remove('fixed');
+        }
+        
+        if(window.screen.height/2 < window.scrollY) {
+            navTop.classList.remove('fixed');
+            this.console.log(window.screen.height/2 < window.scrollY);
+        }
+    });
+
+
+
+
+}
+
+
+
+
+
+
 // $(document).ready(function(){
 
 // $(document).scroll(function(){
-//  if(scrolled=159){
+//  if(window.scrolled>=159){
 //     $("#fixed").addClass( 'fixed');
 //  }
 // });
